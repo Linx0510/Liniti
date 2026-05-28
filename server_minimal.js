af396d4bac2e78c90ee5de9ccace17648d808446
Linx0510 <Linx05@yandex.ru>
2026-05-28 21:33:07 +0500

    ьтб

--- server_minimal.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/test123', (req, res) => {
-  res.send('test123 from minimal server');
-});
-
-app.listen(3007, () => console.log('Minimal on 3007'));
--- test_server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/hello', (req, res) => {
-  console.log('HELLO');
-  res.send('hello');
-});
-
-app.listen(3002, () => console.log('Test on 3002'));
